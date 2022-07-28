220c454bf06f41da15e4a0b2d9711a103d5c1229
atch2203 <atch2203@mit.edu>
2022-07-28 16:28:57 -0400

    switch startup.sh to final file

--- startup.sh
+++ startup.sh
@@ -5,7 +5,7 @@ echo "$timestamp" > $HOME/boot.txt
 sleep 10s
 sudo hciconfig hci0 piscan
 touch $HOME/donesleep.txt
-python3 $HOME/CHARMS/bootbt.py raspberrypi4 True &
+python3 $HOME/CHARMS/cubesat.py raspberrypi4 True &
 touch $HOME/ranthing.txt
 sleep 3s
-python3 $HOME/CHARMS/bootbt.py raspberrypi4 True
+python3 $HOME/CHARMS/cubesat.py raspberrypi4 False 
