0fe647126fa026360be87876a855dcf72cc28336
atch2203 <atch2203@mit.edu>
2022-07-28 14:10:11 -0400

    rename run-on-startup

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 sleep 10s
 sudo hciconfig hci0 piscan
 touch $HOME/donesleep.txt
-python3 $HOME/CHARMS/bootbt.py raspberrypi4 True &
+python3 $HOME/CHARMS/bootbt.py raspberrypi3 True &
 touch $HOME/ranthing.txt
 sleep 3s
-python3 $HOME/CHARMS/bootbt.py raspberrypi4 True
+python3 $HOME/CHARMS/bootbt.py raspberrypi3 True
