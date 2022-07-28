22bef6159eef2e9953f062d74d48b6158c28e8cd
atch2203 <atch2203@mit.edu>
2022-07-28 12:03:42 -0400

    Add main to inits

--- groundinit.py
+++ groundinit.py
@@ -3,12 +3,16 @@ import sys
 import traceback
 from git_push import commit_and_push 
 
-other_pi = sys.argv[1]
-first_time = sys.argv[2]
-connection = BTCon(other_pi)
-if first_time == "True":
-    connection.connect_repeat_as_client(1, 5)
-connection.connect_as_host(1)
-print(connection.receive_string())
-connection.write_string("hi back")
-connection.close_all_connections()
+def main():
+    other_pi = sys.argv[1]
+    first_time = sys.argv[2]
+    connection = BTCon(other_pi)
+    if first_time == "True":
+        connection.connect_repeat_as_client(1, 5)
+    connection.connect_as_host(1)
+    print(connection.receive_string())
+    connection.write_string("hi back")
+    connection.close_all_connections()
+
+if __name__ == "__main__":
+    main()
