c61fc83c9cbf060e95a1ac20ac68933c6f81ae9f
atch2203 <atch2203@mit.edu>
2022-07-28 14:15:22 -0400

    Revert "Merge pull request #1 from BWSI-CubeSat2022/comms"
    This reverts commit 928bc874323866476bb754acb505e096651fadad, reversing
    changes made to 439ff02d44a2688e3bab56901bccd2b0cc07e99f.

--- startup.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/bash
-sleep 10s
-sudo hciconfig hci0 piscan
-touch $HOME/donesleep.txt
-python3 $HOME/CHARMS/bootbt.py raspberrypi4 True &
-touch $HOME/ranthing.txt
-sleep 3s
-python3 $HOME/CHARMS/bootbt.py raspberrypi4 True
