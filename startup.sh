5b24de5ea8c7707a23e5026b0e997212b0760337
atch2203 <atch2203@mit.edu>
2022-07-29 15:32:20 -0400

    remove boot.txt on boot

--- startup.sh
+++ startup.sh
@@ -1,7 +1,4 @@
 #! /usr/bin/bash
-timestamp=$(date +%c)
-touch $HOME/boot.txt
-echo "$timestamp" > $HOME/boot.txt
 sleep 10s
 sudo hciconfig hci0 piscan
 python3 $HOME/CHARMS/cubesat.py raspberrypi4 True > printlog.txt &
