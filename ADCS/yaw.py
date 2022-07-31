ed5dfa39122e56cf054730552de39b7999362fa8
hannahlee <lee_han@mit.edu>
2022-07-31 10:22:16 -0400

    simplified yaw plot code and tried to improve offset

--- ADCS/yaw.py
+++ ADCS/yaw.py
@@ -48,44 +48,25 @@ class KeyListener:
             result = True
         return result
 
-def roll_am(accelX,accelY,accelZ):
+def yaw_am(magX,magY):
     #TODO
-    roll = np.arctan2(accelY, np.sqrt(np.power(accelX, 2) + np.power(accelZ, 2)))
-    return (180/np.pi) * roll
-
-def pitch_am(accelX,accelY,accelZ):
-    #TODO
-    pitch = np.arctan2(accelX, np.sqrt(np.power(accelY, 2) + np.power(accelZ, 2)))
-    return (180/np.pi) * pitch
-
-def yaw_am(accelX,accelY,accelZ,magX,magY,magZ):
-    #TODO
-    #mag_x = magX * np.cos(pitch_am(accelX, accelY, accelZ)) + magY * np.sin(roll_am(accelX, accelY, accelZ)) + magZ * np.cos(roll_am(accelX, accelY, accelZ)) * np.sin(pitch_am(accelX, accelY, accelZ))
-    #mag_y = magY * np.cos(roll_am(accelX, accelY, accelZ)) - magY * np.sin(roll_am(accelX, accelY, accelZ))
-    #return (180/np.pi)*np.arctan2(-mag_y, mag_x)
-
     return (180/np.pi)*np.arctan2(magY, magX)
-
-def yaw_gy(prev_angle, delT, gyro, accDataZ):
-    #TODO
-    #yaw = prev_angle + gyro * delT
-    yaw = 0.95 * (prev_angle + gyro * delT) + (0.05) * (accDataZ)
-    return yaw
 
 def set_initial(mag_offset):
     #Sets the initial position for plotting and gyro calculations.
     print("Preparing to set initial angle. Please hold the IMU still.")
     time.sleep(5)
     print("Setting angle...")
-    accelX, accelY, accelZ = sensor1.accelerometer #m/s^2
     magX, magY, magZ = sensor1.magnetometer #gauss
     #Calibrate magnetometer readings. Defaults to zero until you
     #write the code
     magX = (magX - mag_offset[0]) * mag_offset[3]
     magY = (magY - mag_offset[1]) * mag_offset[4]
     magZ = (magZ - mag_offset[2]) * mag_offset[5]
-    yaw = yaw_am(accelX,accelY,accelZ,magX,magY,magZ)
+    yaw = yaw_am(magX,magY)
     print("Initial angle set.")
+    time.sleep(2)
+    print("Initial Angle: ")
     print(yaw)
     return yaw
 
@@ -127,6 +108,7 @@ def calibrate_mag():
         offset_y = (max_y + min_y) / 2
         offset_z = (max_z + min_z) / 2
 
+        #soft iron calibration
         avg_delta_x = (max_x - min_x) / 2
         avg_delta_y = (max_y - min_y) / 2
         avg_delta_z = (max_z - min_z) / 2
@@ -159,67 +141,3 @@ def calibrate_mag():
 
     print(mag_calibration)
     return mag_calibration
-
-def calibrate_gyro():
-    key_listener = KeyListener()
-    key_listener.start()
-    #TODO
-    offset = [0, 0, 0]
-
-    gyro_x, gyro_y, gyro_z = sensor2.gyroscope
-    min_x = max_x = gyro_x
-    min_y = max_y = gyro_y
-    min_z = max_z = gyro_z
-
-    print("")
-    print("")
-    print("Gyro Calibration")
-    print("Place your gyro on a FLAT stable surface.")
-    print("Press ENTER to continue...")
-    while not key_listener.pressed:
-        pass
-
-    for _ in range(SAMPLE_SIZE):
-        gyro_x, gyro_y, gyro_z = sensor2.gyroscope
-
-        print(
-            "Gyroscope: X: {0:8.2f}, Y:{1:8.2f}, Z:{2:8.2f} rad/s".format(
-                gyro_x, gyro_y, gyro_z
-            )
-        )
-
-        min_x = min(min_x, gyro_x)
-        min_y = min(min_y, gyro_y)
-        min_z = min(min_z, gyro_z)
-
-        max_x = max(max_x, gyro_x)
-        max_y = max(max_y, gyro_y)
-        max_z = max(max_z, gyro_z)
-
-        offset_x = (max_x + min_x) / 2
-        offset_y = (max_y + min_y) / 2
-        offset_z = (max_z + min_z) / 2
-
-        noise_x = max_x - min_x
-        noise_y = max_y - min_y
-        noise_z = max_z - min_z
-
-        print(
-            "Zero Rate Offset:  X: {0:8.2f}, Y:{1:8.2f}, Z:{2:8.2f} rad/s".format(
-                offset_x, offset_y, offset_z
-            )
-        )
-        print(
-            "Rad/s Noise:       X: {0:8.2f}, Y:{1:8.2f}, Z:{2:8.2f} rad/s".format(
-                noise_x, noise_y, noise_z
-            )
-        )
-        print("")
-
-    gyro_calibration = (offset_x, offset_y, offset_z)
-    offset[0] = (180/np.pi) * offset_x 
-    offset[1] = (180/np.pi) * offset_y
-    offset[2] = (180/np.pi) * offset_z
-
-    print("Calibration complete.")
-    return offset
--- ADCS/yaw_plot.py
+++ ADCS/yaw_plot.py
@@ -23,43 +23,19 @@ y = []
 
 rpy = [0,0,0]
 
-def animate(i, xs, type, y, mag_offset, gyro_offset, initial_angle):
-    if len(y) == 0:
-        prev_ang = 0
-    else:
-        c = y[-1]
-        prev_ang = c
-        
-    accelX, accelY, accelZ = sensor1.accelerometer #m/s^2
+def animate(i, xs, type, y, mag_offset, initial_angle):
     magX, magY, magZ = sensor1.magnetometer #gauss
     #Calibrate magnetometer readings
     magX = (magX - mag_offset[0]) * mag_offset[3]
     magY = (magY - mag_offset[1]) * mag_offset[4] 
-    magZ = (magZ - mag_offset[2]) * mag_offset[5] 
-    accgyroX, accgyroY, accgyroZ = sensor2.gyroscope #rad/s
-    gyroX = accgyroX * (180/np.pi) - gyro_offset[0]
-    gyroY = accgyroY * (180/np.pi) - gyro_offset[1]
-    gyroZ = accgyroZ * (180/np.pi) - gyro_offset[2]
+    magZ = (magZ - mag_offset[2]) * mag_offset[5]
     xs.append(time.time())
 
     if type == 'am':
-       y.append(yaw_am(accelX,accelY,accelZ,magX,magY,magZ) - initial_angle)
+       y.append(yaw_am(magX, magY) - initial_angle)
        ax.clear()
        ax.plot(xs,y,label = "Yaw")
        plt.title('Yaw, Using Accelerometer and Magnetometer')
-       plt.ylabel('deg')
-
-    elif type =='gyro':
-       if len(xs) == 1:
-           y.append(prev_ang)
-
-       else:
-           delT = xs[-1] - xs[-2]
-           y.append(yaw_gy(prev_ang,delT,gyroZ,accgyroZ))
-
-       ax.clear()
-       ax.plot(xs,y,label = "Yaw")
-       plt.title('Yaw, Using Gyro')
        plt.ylabel('deg')
 
     else:
@@ -76,8 +52,7 @@ def animate(i, xs, type, y, mag_offset, gyro_offset, initial_angle):
 def plot_data(type = 'am'):
     mag_offset = calibrate_mag()
     initial_angle = set_initial(mag_offset)
-    gyro_offset = calibrate_gyro()
-    ani = animation.FuncAnimation(fig, animate, fargs =(xs,type,y,mag_offset,gyro_offset,initial_angle), interval = 1000)
+    ani = animation.FuncAnimation(fig, animate, fargs =(xs,type,y,mag_offset,initial_angle), interval = 1000)
     plt.show()
 
 if __name__ == '__main__':
