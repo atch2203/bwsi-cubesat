8061623a8a269be6d148412f3692bafb2c14c860
atch2203 <atch2203@mit.edu>
2022-08-04 13:35:08 -0400

    add try except to git push/pull

--- Comms/git_push.py
+++ Comms/git_push.py
@@ -1,17 +1,23 @@
 from git import Repo
 
 def commit_and_push(message):
-    repo = Repo('/home/pi/CHARMS')
-    repo.git.add('--all')
-    repo.index.commit(f"-Automatic push- {message}")
-    print('made the commit')
-    origin = repo.remote('origin')
-    print('added remote')
-    origin.push()
-    print('pushed changes\n')
+    try:
+        repo = Repo('/home/pi/CHARMS')
+        repo.git.add('--all')
+        repo.index.commit(f"-Automatic push- {message}")
+        print('made the commit')
+        origin = repo.remote('origin')
+        print('added remote')
+        origin.push()
+        print('pushed changes\n')
+    except:
+        print("error in pushing")
 
 def pull():
-    repo = Repo('/home/pi/CHARMS')
-    origin = repo.remote('origin')
-    origin.pull()
-    print('pulled')
+    try:
+        repo = Repo('/home/pi/CHARMS')
+        origin = repo.remote('origin')
+        origin.pull()
+        print('pulled')
+    except:
+        print('error in pulling')
